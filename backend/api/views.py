9f33cf4845da19699bf2e6241182f79ada45eada
artem41k <waw0905@mail.ru>
2025-02-04 22:32:18 +0500

    add jwt authorization using http-only cookies

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
 
-# Create your views here.
+from . import models, serializers
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        original_resp = super().post(request, *args, **kwargs)
+
+        if 'access' in original_resp.data and 'refresh' in original_resp.data:
+            response = Response(status=status.HTTP_200_OK)
+            response.set_cookie(
+                key='access_token', value=original_resp.data['access'],
+                httponly=True, secure=True, samesite='lax',
+                max_age=settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME']
+            )
+            response.set_cookie(
+                key='refresh_token', value=original_resp.data['refresh'],
+                httponly=True, secure=True, samesite='lax',
+                max_age=settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME']
+            )
+            return response
+
+        return original_resp
+
+
+class CreateUserView(CreateAPIView):
+    serializer_class = serializers.CreateUserSerializer
+
+
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.UserSerializer
+
+
+class FormView(ModelViewSet):
+    serializer_class = serializers.FormSerializer
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
 import os
 
 load_dotenv()
@@ -117,3 +118,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'api.User'
+
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
+}
+
+
+HTTPS = bool(int(os.getenv('HTTPS')))
+
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = HTTPS
